c3bd1f82152f4a9c42a1eee48a3f15b53b7c1486
eugenGirlescu <61660109+eugenGirlescu@users.noreply.github.com>
2020-08-31 14:54:23 +0300

    Error pages

--- src/main/java/com/car/carDealer/service/FileService.java
+++ src/main/java/com/car/carDealer/service/FileService.java
@@ -15,7 +15,7 @@ import java.nio.file.StandardCopyOption;
 @Service
 public class FileService {
 
-    @Value("${app.upload.dir:${user.home}}")
+   git branch
     public String uploadDir;
 
     public void uploadFile(MultipartFile file) {
